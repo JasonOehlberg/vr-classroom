e1e3f765d500e294b936a70560f8b631915f7279
Jason Oehlberg <jt.oehlberg@gmail.com>
2018-11-01 17:17:13 -0500

    Modify StudentBehavior script + Modify BlendWriting Animation

--- Assets/MyClassroom/Scripts/StudentBehavior.cs
+++ Assets/MyClassroom/Scripts/StudentBehavior.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 using UnityEngine.Windows.Speech;
 using System.Linq;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class StudentBehavior : MonoBehaviour {
 
+    public List<GameObject> students;
     KeywordRecognizer keywordRecognizer;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+    Animator animator;
+    string called = "";
+    
     // Use this for initialization
     void Start () {
-		
+        students = new List<GameObject>();
+
+        foreach (GameObject student in GameObject.FindGameObjectsWithTag("Student"))
+        {
+            students.Add(student);
+        }
+        foreach (GameObject s in students)
+        {
+            keywords.Add(s.name, () => {
+                foreach (GameObject student in students)
+                {
+                    if (student.name.Equals(called))
+                    {
+                        Debug.Log("Im here" + student.name);
+                        student.GetComponent<Animator>().SetBool("IsCalled", true);
+                        
+                    }
+                   
+                }
+                
+            });
+        }
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            Debug.Log(args.text);
+            called = args.text;
+            keywordAction.Invoke();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
